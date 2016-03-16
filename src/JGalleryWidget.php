f38b4af26065388782b34ee52bdb586b5fdc6bfb
Jakeroid <jakeroid.s@gmail.com>
2016-03-16 13:57:48 +0200

    Added basic widget.

---
+++ src/JGalleryWidget.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Jakeroid
+ * Date: 16-Mar-16
+ * Time: 13:10
+ */
+
+namespace jakeroid\yii2\widgets;
+
+use yii\base\Widget;
+
+class JGalleryWidget extends Widget
+{
+    public $request_url;
+    public $custom_fields;
+
+    public function init()
+    {
+
+    }
+
+    public function run()
+    {
+
+    }
+}
