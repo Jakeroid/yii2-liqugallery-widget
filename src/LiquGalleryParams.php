44e0b778a67c018af50dabbfe9105d7da09acf7f
Jakeroid <jakeroid.s@gmail.com>
2018-05-23 12:52:07 +0300

    added new param; updated gitignore

--- src/LiquGalleryParams.php
+++ src/LiquGalleryParams.php
@@ -13,6 +13,7 @@ class LiquGalleryParams {
     const ACTION = 'action';
     const UPLOADED_IMAGES = 'images';
     const IMAGE_ID = 'image_id';
+    const IMAGE_TYPE = 'image_type';
     const GALLERY_GROUP_ID = 'gallery_group_id';
     const TAG_NAME = 'tag_name';
     const TAG_VALUE = 'tag_value';
